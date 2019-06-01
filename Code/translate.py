9a0bfe79e255e7b6c3fafdca45e7cac07c94eb31
e0031374 <e0031374@u.nus.edu>
2019-06-01 20:09:58 +0800

    Update translate.py
    Edited to ensure that changes are written when there are changes instead of after the loop finishe the range,
    
    Removed  the part where "tweet" column to be copied to "translated" column each time the program starts
    
    it will now read from "tweet" and translate and write into "translated" instead (assuming the changes work)
    
    also please change the paths in pd.read_csv and uncorrupted.to_csv before using, i should really make it a single variable thing next time

--- Code/translate.py
+++ Code/translate.py
@@ -2,17 +2,24 @@ import pandas as pd
 from time import sleep
 from googletrans import Translator
 
-uncorrupted = pd.read_csv('Data/scraped/translated.csv')
-tr = Translator()    #ard 893
-uncorrupted["translated"] = uncorrupted["tweet"]
+uncorrupted = pd.read_csv('/home/owo/Downloads/isextreme-master/Translate/translated.csv')
+tr = Translator()    #ard 893 #did from 2394-3030
+#uncorrupted["translated"] = uncorrupted["tweet"] #i think this reverts any progress between sessions?
 for i in range(2394, 6000):
     try:
-        uncorrupted["translated"].iloc[i] = tr.translate(uncorrupted["translated"].iloc[i]).text
+        original = uncorrupted["tweet"].iloc[i]
+        uncorrupted["translated"].iloc[i] = tr.translate(uncorrupted["tweet"].iloc[i]).text
+        same = original == uncorrupted["translated"].iloc[i]
         print(uncorrupted["translated"][i])
         print(i)
-        sleep(5)
+        if not same:
+            print("Translated! (maybe!)")
+            uncorrupted.to_csv('/home/owo/Downloads/isextreme-master/Translate/translated.csv',encoding='utf-8-sig')
+            print("Write Success! " + str(i) + ": " + uncorrupted["translated"].iloc[i])
+        sleep(3)
     except Exception:
+        print("EXCEPTION: " + uncorrupted["translated"][i])
         continue
 
 
-uncorrupted.to_csv('Data/scraped/translated.csv',encoding='utf-8-sig') # thisworks
+uncorrupted.to_csv('/home/owo/Downloads/isextreme-master/Translate/translated.csv',encoding='utf-8-sig') # thisworks
