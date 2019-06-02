b1bc3f1788e38b04ba6b5ac36ac499b6376b8df4
e0031374 <e0031374@u.nus.edu>
2019-06-02 22:11:43 +0800

    Update translate.py
    made it a bit neater so its easier to keep track of things

--- Code/translate.py
+++ Code/translate.py
@@ -2,24 +2,38 @@ import pandas as pd
 from time import sleep
 from googletrans import Translator
 
-uncorrupted = pd.read_csv('/home/owo/Downloads/isextreme-master/Translate/translated.csv')
-tr = Translator()    #ard 893 #did from 2394-3030
+PATHFILE = '/home/owo/Downloads/isextreme-master/Translate/translated.csv'
+STARTROW = 1
+ENDROW = 2395
+
+
+uncorrupted = pd.read_csv(PATHFILE)
+tr = Translator()    #ard 893 #did from 2394-3030, 3030-8500, 8500-10938
 #uncorrupted["translated"] = uncorrupted["tweet"] #i think this reverts any progress between sessions?
-for i in range(2394, 6000):
+for i in range(STARTROW, ENDROW):
     try:
+        print("Begin: " + str(i))
         original = uncorrupted["tweet"].iloc[i]
-        uncorrupted["translated"].iloc[i] = tr.translate(uncorrupted["tweet"].iloc[i]).text
-        same = original == uncorrupted["translated"].iloc[i]
-        print(uncorrupted["translated"][i])
-        print(i)
+        translated = tr.translate(uncorrupted["tweet"].iloc[i]).text
+        same = original == translated
+
+        #writes google translate output to dataframe
+        uncorrupted["translated"].iloc[i] = translated 
+
         if not same:
             print("Translated! (maybe!)")
-            uncorrupted.to_csv('/home/owo/Downloads/isextreme-master/Translate/translated.csv',encoding='utf-8-sig')
+            #writing dataframe to csv to save progress
+            uncorrupted.to_csv(PATHFILE,encoding='utf-8-sig')
             print("Write Success! " + str(i) + ": " + uncorrupted["translated"].iloc[i])
+        else:
+            print("no translated required: ")
+            print(uncorrupted["translated"][i])
         sleep(3)
     except Exception:
         print("EXCEPTION: " + uncorrupted["translated"][i])
+        print()
         continue
+    print() # newline makes it easier to see
 
 
-uncorrupted.to_csv('/home/owo/Downloads/isextreme-master/Translate/translated.csv',encoding='utf-8-sig') # thisworks
+uncorrupted.to_csv(PATHFILE,encoding='utf-8-sig') # thisworks
